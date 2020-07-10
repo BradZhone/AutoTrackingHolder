93af3445f71546b74002d818935addffef486658
BradZhone <49404479+BradZhone@users.noreply.github.com>
2020-07-11 00:20:31 +0800

    ver3.0 增加了人脸识别追踪功能，但是每次控制舵机转动10度，存在卡顿现象，且存在多个目标时无法确定跟踪对象

--- app/src/main/java/com/client/myapplication/client/MainActivity.java
+++ app/src/main/java/com/client/myapplication/client/MainActivity.java
@@ -1,10 +1,15 @@
 package com.client.myapplication.client;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
@@ -13,22 +18,23 @@ import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.io.PrintWriter;
 import java.net.Socket;
 
 @SuppressLint("SetTextI18n")
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     Thread Thread1 = null;      //线程1用于建立连接
     EditText etIP, etPort;      //服务器ip、端口输入框
     static TextView tvMessages;     //消息框内容
     static EditText etMessage;      //编辑框
-    Button btnSend, btnUp, btnDown, btnLeft, btnRight, btnOp, btnTracking;
-    Switch aSwitch;
+    Button btnSend, btnUp, btnDown, btnLeft, btnRight, btnOp, btnTracking;  //控制按键
+    Switch switch_connect; //连接开关
 
-    String SERVER_IP;   //服务器ip
-    int SERVER_PORT;    //服务器端口
+    String IP = "192.168.1.1";  //服务器ip(默认)
+    String port = "8266";    //服务器端口（默认）
+    String SERVER_IP;   //服务器ip(可自定义)
+    int SERVER_PORT;    //服务器端口（可自定义）
 
     boolean switchOn = false;     //是否开启连接
     Socket socket;      // 建立socket连接
@@ -42,8 +48,8 @@ public class MainActivity extends AppCompatActivity {
         //ip和端口
         etIP = findViewById(R.id.etIP);
         etPort = findViewById(R.id.etPort);
-        etIP.setText("192.168.1.1");
-        etPort.setText("8266");
+        etIP.setText(IP);
+        etPort.setText(port);
 
         tvMessages = findViewById(R.id.tvMessages);
         tvMessages.setMovementMethod(ScrollingMovementMethod.getInstance());
@@ -59,14 +65,14 @@ public class MainActivity extends AppCompatActivity {
 
         //跟踪模式按键
         btnTracking = findViewById(R.id.Tracking);
-        aSwitch = findViewById(R.id.switch_connect);
+        switch_connect = findViewById(R.id.switch_connect);
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String message = etMessage.getText().toString().trim();
                 if (!switchOn){
-                    ToastUtil.showToast(MainActivity.this, "请先连接服务器再进行本操作");
+                    ToastUtil.showToast(MainActivity.this, "请先连接云台再进行本操作");
                 }else{
                     new Thread(new Thread2(message)).start();
                 }
@@ -77,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String message = "uuuuuuuuu";
                 if (!switchOn){
-                    ToastUtil.showToast(MainActivity.this, "请先连接服务器再进行本操作");
+                    ToastUtil.showToast(MainActivity.this, "请先连接云台再进行本操作");
                 }else{
                     new Thread(new Thread2(message)).start();
                 }
@@ -88,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String message = "ddddddddd";
                 if (!switchOn){
-                    ToastUtil.showToast(MainActivity.this, "请先连接服务器再进行本操作");
+                    ToastUtil.showToast(MainActivity.this, "请先连接云台再进行本操作");
                 }else{
                     new Thread(new Thread2(message)).start();
                 }
@@ -99,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String message = "lllllllll";
                 if (!switchOn){
-                    ToastUtil.showToast(MainActivity.this, "请先连接服务器再进行本操作");
+                    ToastUtil.showToast(MainActivity.this, "请先连接云台再进行本操作");
                 }else{
                     new Thread(new Thread2(message)).start();
                 }
@@ -110,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String message = "rrrrrrrrr";
                 if (!switchOn){
-                    ToastUtil.showToast(MainActivity.this, "请先连接服务器再进行本操作");
+                    ToastUtil.showToast(MainActivity.this, "请先连接云台再进行本操作");
                 }else{
                     new Thread(new Thread2(message)).start();
                 }
@@ -121,13 +127,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String message = "op";
                 if (!switchOn){
-                    ToastUtil.showToast(MainActivity.this, "请先连接服务器再进行本操作");
+                    ToastUtil.showToast(MainActivity.this, "请先连接云台再进行本操作");
                 }else{
                     new Thread(new Thread2(message)).start();
                 }
             }
         });
-        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        switch_connect.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked && !switchOn){
@@ -142,7 +148,11 @@ public class MainActivity extends AppCompatActivity {
                     //未选中状态 且选过一次 且选过的那次连接成功
                     try {
                         socket.close();
-                        tvMessages.append("Disconnected from server!\n");
+                        tvMessages.append(" Disconnected from server!\n");
+                        int offset=tvMessages.getLineCount()*tvMessages.getLineHeight();
+                        if(offset>tvMessages.getHeight()-tvMessages.getLineHeight()-20){
+                            tvMessages.scrollTo(0,offset-tvMessages.getHeight()+tvMessages.getLineHeight()+20);
+                        }
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
@@ -151,41 +161,61 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
     public void TrackingMode(View view){
-        //切换跟随模式
-        Intent intent = new Intent(this, camera.class);
-        startActivity(intent);
+        //判断是否有权限并切换至跟随模式
+        if (!switchOn){
+            ToastUtil.showToast(MainActivity.this, "请先连接云台再进行本操作");
+        }else{
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA}, 1);
+            } else {
+                startActivity(new Intent(this, camera.class));
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //判断权限
+        if (requestCode == 1) {
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startActivity(new Intent(this, camera.class));
+            } else {
+                Toast.makeText(this, "权限拒绝", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     public static class ToastUtil {
-
+        //自动滚动文本框
         private static Toast toast;
-
         @SuppressLint("ShowToast")
         public static void showToast(Context context,
                                      String content) {
             if (toast == null) {
                 toast = Toast.makeText(context, content, Toast.LENGTH_LONG);
             } else {
-//                toast.setText(content);
                 toast.cancel();
                 toast = Toast.makeText(context, content, Toast.LENGTH_LONG);
             }
             toast.show();
         }
-
     }
 
-
     public class Thread1 implements Runnable {
+        //建立TCP
         @Override
         public void run() {
             try {
                 socket = new Socket(SERVER_IP, SERVER_PORT);
                 output = new PrintWriter(socket.getOutputStream());
-                tvMessages.append("Connected to server!\n");
-
+                tvMessages.append(" Connected to server!\n");
+                int offset=tvMessages.getLineCount()*tvMessages.getLineHeight();
+                if(offset>tvMessages.getHeight()-tvMessages.getLineHeight()-20){
+                    tvMessages.scrollTo(0,offset-tvMessages.getHeight()+tvMessages.getLineHeight()+20);
+                }
             } catch (Exception e) {
                 e.printStackTrace();
 
@@ -194,20 +224,24 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     public static class Thread2 implements Runnable {
         //线程2用于发送数据至服务器
         private String message;
-
         Thread2(String message) {
             this.message = message;
         }
-
         @Override
         public void run() {
+            //发送数据至服务器
             output.write(message);
             output.flush();
-            tvMessages.append("client: " + message + "\n");
+            //在文本框中显示发送内容
+            tvMessages.append(" Send message: " + message + "\n");
+            int offset=tvMessages.getLineCount()*tvMessages.getLineHeight();
+            if(offset>tvMessages.getHeight()-tvMessages.getLineHeight()-20){
+                tvMessages.scrollTo(0,offset-tvMessages.getHeight()+tvMessages.getLineHeight()+20);
+            }
+            //清空发送数据框
             etMessage.setText("");
         }
     }
--- app/src/main/java/com/client/myapplication/client/camera.java
+++ app/src/main/java/com/client/myapplication/client/camera.java
@@ -1,79 +1,254 @@
 package com.client.myapplication.client;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.OrientationEventListener;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Arrays;
 
+public class camera extends AppCompatActivity implements
+        CameraBridgeViewBase.CvCameraViewListener2, View.OnClickListener {
+    private CameraBridgeViewBase cameraView;
+    private CascadeClassifier classifier;
+    private Mat mGray;
+    private Mat mRgba;
+    private int mAbsoluteFaceSize = 0;
+    private boolean isFrontCamera;
+    private OrientationEventListener mOreientationListener;
+    public float[] center = new float[2];
+    public float[] width_range = new float[2];
+    public float[] height_range = new float[2];
 
-public class camera extends AppCompatActivity {
-    Button btnUp, btnDown, btnLeft, btnRight, btnOp;
+    private int x=0;//统计已经发送出的命令数，防止转到90度还一直发
+    private int y=0;
+    private int time = 0; //计算检测到人脸超出框的时间，防止发出过多命令
+
+    // 手动装载openCV库文件，以保证手机无需安装OpenCV Manager
+    static {
+        System.loadLibrary("opencv_java3");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initWindowSettings();
         setContentView(R.layout.activity_camera);
+        cameraView = findViewById(R.id.camera_view);
+        cameraView.setCvCameraViewListener(this); // 设置相机监听
+        initClassifier();
+        cameraView.enableView();
+        Button switchCamera = findViewById(R.id.switch_camera);
+        switchCamera.setOnClickListener(this); // 切换相机镜头，默认后置
 
-        //设置返回键
-        ActionBar actionBar = getSupportActionBar();
-        if(actionBar != null){
-            actionBar.setHomeButtonEnabled(true);
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-
-        //方向控制按键
-        btnUp = findViewById(R.id.button_up);
-        btnDown = findViewById(R.id.button_down);
-        btnLeft = findViewById(R.id.button_left);
-        btnRight = findViewById(R.id.button_right);
-        btnOp = findViewById(R.id.button_op);
-
-        btnUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String message = "uuuuuuuuu";
-                new Thread(new MainActivity.Thread2(message)).start();
-            }
-        });
-        btnDown.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String message = "ddddddddd";
-                new Thread(new MainActivity.Thread2(message)).start();
-            }
-        });
-        btnLeft.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String message = "lllllllll";
-                new Thread(new MainActivity.Thread2(message)).start();
-            }
-        });
-        btnRight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String message = "rrrrrrrrr";
-                new Thread(new MainActivity.Thread2(message)).start();
-            }
-        });
-        btnOp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String message = "op";
-                new Thread(new MainActivity.Thread2(message)).start();
-            }
-        });
-
+        DisplayMetrics metric = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metric);
+        int width = metric.widthPixels;  // 屏幕宽度（像素）
+        int height = metric.heightPixels;  // 屏幕高度（像素）
+        Log.v("screen_width", String.valueOf(width));
+        Log.v("screen_height", String.valueOf(height));
+        width_range = new float[]{(float) (width * 2 * 0.85 / 5.0), (float) (width * 3 * 0.81 / 5.0)};
+        height_range = new float[]{(float) (height * 2 * 0.836 / 5.0), (float) (height * 3/ 5.0)};
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            this.finish();
-            return true;
+    public void onClick(View v) {
+        if (v.getId() == R.id.switch_camera) {
+            cameraView.disableView();
+            if (isFrontCamera) {
+                cameraView.setCameraIndex(CameraBridgeViewBase.CAMERA_ID_BACK);
+                isFrontCamera = false;
+            } else {
+                cameraView.setCameraIndex(CameraBridgeViewBase.CAMERA_ID_FRONT);
+                isFrontCamera = true;
+            }
+            cameraView.enableView();
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    // 初始化窗口设置, 包括全屏、横屏、常亮
+    @SuppressLint("SourceLockedOrientationActivity")
+    private void initWindowSettings() {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+    }
+
+    // 初始化人脸级联分类器，必须先初始化
+    private void initClassifier() {
+        try {
+            InputStream is = getResources()
+                    .openRawResource(R.raw.haarcascade_frontalface_alt);
+            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+            File cascadeFile = new File(cascadeDir, "haarcascade_frontalface_alt.xml");
+            FileOutputStream os = new FileOutputStream(cascadeFile);
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            os.close();
+            classifier = new CascadeClassifier(cascadeFile.getAbsolutePath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mGray = new Mat();
+        mRgba = new Mat();
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mGray.release();
+        mRgba.release();
+    }
+
+    @Override
+    // 这里执行人脸检测的逻辑, 根据OpenCV提供的例子实现(face-detection)
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        mGray = inputFrame.gray();
+        // 翻转矩阵以适配前后置摄像头
+        if (isFrontCamera) {
+            Core.flip(mRgba, mRgba, 1);     //朝右翻转
+            Core.flip(mGray, mGray, 1);     //朝右翻转
+        }
+        float mRelativeFaceSize = 0.2f;
+        if (mAbsoluteFaceSize == 0) {
+            int height = mGray.rows();
+            if (Math.round(height * mRelativeFaceSize) > 0) {
+                mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
+            }
+        }
+        MatOfRect faces = new MatOfRect();
+        if (classifier != null)
+            classifier.detectMultiScale(mGray, faces, 1.1, 2, 2,
+                    new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
+        Rect[] facesArray = faces.toArray();
+
+        Scalar rangeRectColor = new Scalar(255, 255, 0, 255);
+        Scalar faceRectColor = new Scalar(0, 255, 0, 255);
+        try{    //尝试寻找人脸并标出第一个人脸
+            //画出边框
+            Point range_st = new Point(width_range[0], height_range[0]);
+            Point range_en = new Point(width_range[1], height_range[1]);
+            Imgproc.rectangle(mRgba, range_st, range_en,rangeRectColor,3);
+            Log.v("screen_width_range", Arrays.toString(width_range));
+            Log.v("screen_height_range", Arrays.toString(height_range));
+
+            Imgproc.rectangle(mRgba,facesArray[0].tl(), facesArray[0].br(),faceRectColor,3);
+            Log.v("Faces", String.valueOf(facesArray[0]));
+            //标出人脸位置
+            if(Math.abs(center[0] - (facesArray[0].x+facesArray[0].width/2.0))<=60 && Math.abs(center[1] - (facesArray[0].y + facesArray[0].height/2.0))<=60){
+                //如果依次检测到的两个矩阵左上角坐标之差小于50，则更新人脸中心坐标
+                center[0] = (float) (facesArray[0].x + (facesArray[0].width/2.0));
+                center[1] = (float) (facesArray[0].y + (facesArray[0].height/2.0));
+                Log.v("face_center", Arrays.toString(center));
+            }else{
+                center[0] = (float) (facesArray[0].x + (facesArray[0].width/2.0));
+                center[1] = (float) (facesArray[0].y + (facesArray[0].height/2.0));
+            }
+
+            //  超出边框时控制云台转动
+            if(time==0) {
+                if (center[0] < width_range[0]) {//超出左边框
+                    if (!isFrontCamera && x < 9) {
+                        Log.v("command_left", "r");
+                        String message = "r";
+                        new Thread(new MainActivity.Thread2(message)).start();
+                        x += 1;
+                    } else if (isFrontCamera && x > -9) {
+                        Log.v("command_right", "l");
+                        String message = "l";
+                        new Thread(new MainActivity.Thread2(message)).start();
+                        x -= 1;
+                    }
+                } else if (center[0] > width_range[1]) {//超出右边框
+                    if (!isFrontCamera && x > -9) {
+                        Log.v("command_right", "l");
+                        String message = "l";
+                        new Thread(new MainActivity.Thread2(message)).start();
+                        x -= 1;
+                    } else if (isFrontCamera && x < 9) {
+                        Log.v("command_left", "r");
+                        String message = "r";
+                        new Thread(new MainActivity.Thread2(message)).start();
+                        x += 1;
+                    }
+                } else if (center[1] < height_range[0]) {//超出上边框
+                    if (!isFrontCamera && y > -9) {
+                        Log.v("command_up", "d");
+                        String message = "d";
+                        new Thread(new MainActivity.Thread2(message)).start();
+                        y -= 1;
+                    } else if (isFrontCamera && y < 9) {
+                        Log.v("command_down", "u");
+                        String message = "u";
+                        new Thread(new MainActivity.Thread2(message)).start();
+                        y += 1;
+                    }
+                } else if (center[1] > height_range[1]) {//超出下边框
+                    if (!isFrontCamera && y < 9) {
+                        Log.v("command_down", "u");
+                        String message = "u";
+                        new Thread(new MainActivity.Thread2(message)).start();
+                        y += 1;
+                    } else if (isFrontCamera && y > -9) {
+                        Log.v("command_up", "d");
+                        String message = "d";
+                        new Thread(new MainActivity.Thread2(message)).start();
+                        y -= 1;
+                    }
+                }
+            }
+
+            time = (time+1)%7;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return mRgba;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (cameraView != null) {
+            cameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cameraView.disableView();
     }
 }
+
